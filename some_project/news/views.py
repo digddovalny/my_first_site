fc578ec41c9bcdfb0c1da6dfc9e561c6adc49130
Your Name <igorkol2000@mail.ru>
2023-10-16 22:01:57 +0300

    add models Articles and html add new

---
+++ some_project/news/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Articles
+
+# Create your views here.
+
+def news_home(request):
+    news = Articles.objects.order_by('-date')
+    return render(request, 'news/news_home.html', {'news': news})
+
+def create(requst):
+    return render(requst, 'news/create.html')
